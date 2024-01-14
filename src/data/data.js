425a956fac62683681de1692644f940953efc9b3
Adriano Martiliano <adrianomartiliano@gmail.com>
2024-01-14 02:23:24 -0300

    Criação do Projeto - Criado o arquivo de dados - Limpado os dados padrões de criação de projeto

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+export const palavras = {
+    carro: ["Pneu", "Volante", "Motor", "Porta", "Farol", "Banco", "Câmbio", "Retrovisor", "Freio", "Para-brisa"],
+    fruta: ["Maçã", "Banana", "Morango", "Uva", "Pêssego", "Abacaxi", "Melancia", "Kiwi", "Manga", "Pera"],
+    parteDoCorpo: ["Cabeça", "Braço", "Perna", "Olho", "Nariz", "Orelha", "Boca", "Mão", "Pé", "Dedo"],
+    esporte: ["Futebol", "Basquete", "Tênis", "Natação", "Golfe", "Handebol", "Ciclismo", "Atletismo", "Vôlei", "Boxe"],
+    alimento: ["Arroz", "Feijão", "Macarrão", "Carne", "Salada", "Frango", "Peixe", "Pizza", "Hamburguer", "Sopa"],
+    computador: ["Teclado", "Mouse", "Monitor", "CPU", "Memória", "Disco Rígido", "Placa de Vídeo", "Processador", "Mousepad", "Webcam"]
+}
