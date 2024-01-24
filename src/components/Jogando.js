ce9fbb10e3778d2b6e0a59c71ae62fd3196918f8
Adriano Martiliano <adrianomartiliano@gmail.com>
2024-01-24 00:27:06 -0300

    CSS da tela do jogo
    Criado a tela base do jogo...

--- src/components/Jogando.js
+++ src/components/Jogando.js
@@ -6,11 +6,35 @@ const Jogando = ( { VerificaPalavra, categoriaEscolhida }) => {
     const categoria = categoriaEscolhida;
     //console.log(categoria);
     return(
-        <div>
-            <h2 className="title">Jogando</h2>
-            <h3>Dica: {categoria}</h3>
-            
-            <button onClick={VerificaPalavra}>FINALIZAR O JOGO</button>
+        <div className="jogando">
+            <p className="pontuacao">
+                Pontuação: <span>000</span>
+            </p>
+            <h3>Dica sobre a palavra: <span className="dica">Dica{categoria}</span></h3>
+
+            <div className="containerPalavra">
+                <span className="letra">A</span>
+                <span className="vazio"></span>
+                <span className="vazio"></span>
+                <span className="vazio"></span>
+                <span className="vazio"></span>
+                <span className="vazio"></span>
+            </div>
+
+            <div className="containerLetra">
+                <p>Acerte a letra...</p>
+                <form>
+                    <input type="text" name="letra" maxLength="1" required/>
+                    <button className="btnOk">Verificar</button>
+                </form>
+            </div>
+
+            <div className="letrasErradas">
+                <p>Letras utilizadas:
+                    <span>a, </span>
+                    <span>b, </span>
+                </p>
+            </div>
         </div>
     );
 }
